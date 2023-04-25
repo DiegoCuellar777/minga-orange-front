0c3139276ddefff3e98a86184d82d9cb02a3dcb2
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-04-25 18:29:49 -0500

    Primeras funcionalidades Carrusel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react"
+
 import mobile from "/images/Group-3599.png"
 import left from "/images/left.png"
 import right from "/images/right.png"
 
+import axios from "axios"
+import apiUrl from "../../api"
+
 export default function Carousel() {
+
+  let [counter, setCounter] = useState(0)
+  console.log(counter);
+  
+  let sumar = ()=> {
+    setCounter(counter+1)
+    if (counter === 100) {
+      setCounter(0)
+    }
+  }
+  let restar = ()=> {
+
+    setCounter(counter-1)
+    if (counter === 0) {
+      setCounter(100)
+    }
+  }
+
+
   return (
     <div className="sm:hidden md:w-[60%] flex justify-between  items-center md:px-[5%]">
-      <img src={left} alt="" />
+      <img src={left} alt="" onClick={restar}/>
       <img src={mobile} alt="" className='sm:hidden md:w-[85%] md:h-[110vh] md:object-fit md:object-cover md:object-center  '/>
-      <img src={right} alt="" />
+      <img src={right} alt="" onClick={sumar}/>
+      <span className="text-white">CONTADOR: {counter} </span>
     </div>
   )
 }
