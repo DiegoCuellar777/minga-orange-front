dfda9fa11ac396daaa7b62fa5a0e83c125c62775
DiegoCuellar777 <112589821+DiegoCuellar777@users.noreply.github.com>
2023-04-26 02:56:31 -0500

    F-05: Navbar y Carousel responsive (#5)

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,20 +1,26 @@
 import { useState, useEffect } from "react"
+import axios from "axios"
+import apiUrl from "../../api"
+
 
 import mobile from "/images/Group-3599.png"
 import left from "/images/left.png"
 import right from "/images/right.png"
 
-import axios from "axios"
-import apiUrl from "../../api"
+
 
 export default function Carousel() {
-
+  useEffect(
+    () => { axios(apiUrl+"carousels").then(res => setImages(res.data.carousels)).catch(err=> console.log(err)) },
+    []   // Array de dependencias vacio ya que necesitamos pushear una unica vez al montarse el componente (y despues esos datos no deberian cambiar)
+  )
+  let [imagenesCarousel, setImages] = useState([])
+  console.log(imagenesCarousel);
   let [counter, setCounter] = useState(0)
-  console.log(counter);
   
   let sumar = ()=> {
     setCounter(counter+1)
-    if (counter === 100) {
+    if (counter === imagenesCarousel.length-1) {
       setCounter(0)
     }
   }
@@ -22,7 +28,7 @@ export default function Carousel() {
 
     setCounter(counter-1)
     if (counter === 0) {
-      setCounter(100)
+      setCounter(imagenesCarousel.length-1)
     }
   }
 
@@ -30,9 +36,8 @@ export default function Carousel() {
   return (
     <div className="sm:hidden md:w-[60%] flex justify-between  items-center md:px-[5%]">
       <img src={left} alt="" onClick={restar}/>
-      <img src={mobile} alt="" className='sm:hidden md:w-[85%] md:h-[110vh] md:object-fit md:object-cover md:object-center  '/>
+      <img src={imagenesCarousel[counter]?.cover_photo} alt="" className='sm:hidden md:w-[85%] md:h-[110vh] md:object-fit md:object-cover md:object-center  '/>
       <img src={right} alt="" onClick={sumar}/>
-      <span className="text-white">CONTADOR: {counter} </span>
     </div>
   )
 }
