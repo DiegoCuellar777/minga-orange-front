1ad6c1499deafdd39de01fde06d46f2815c036f2
DiegoCuellar777 <112589821+DiegoCuellar777@users.noreply.github.com>
2023-04-26 11:06:08 -0500

    Sprint 1 (#7)
    * Primeras funcionalidades Carrusel
    
    * F-05: Navbar y Carousel responsive (#5)
    
    * details

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,34 +10,43 @@ import right from "/images/right.png"
 
 
 export default function Carousel() {
+
   useEffect(
     () => { axios(apiUrl+"carousels").then(res => setImages(res.data.carousels)).catch(err=> console.log(err)) },
     []   // Array de dependencias vacio ya que necesitamos pushear una unica vez al montarse el componente (y despues esos datos no deberian cambiar)
   )
+
+
+  let [imagenesCarousel, setImages] = useState([])
+  const [currentImage, setCurrentImage] = useState(0);
+
+
+useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((currentImage) => {
+        if (currentImage === imagenesCarousel.length - 1) {
+          return 0;
+        } else {
+          return currentImage + 1;
+        }
+      });
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [imagenesCarousel.length]);
+
   let [imagenesCarousel, setImages] = useState([])
   console.log(imagenesCarousel);
   let [counter, setCounter] = useState(0)
   
-  let sumar = ()=> {
-    setCounter(counter+1)
-    if (counter === imagenesCarousel.length-1) {
-      setCounter(0)
-    }
-  }
-  let restar = ()=> {
 
-    setCounter(counter-1)
-    if (counter === 0) {
-      setCounter(imagenesCarousel.length-1)
-    }
-  }
 
 
   return (
     <div className="sm:hidden md:w-[60%] flex justify-between  items-center md:px-[5%]">
-      <img src={left} alt="" onClick={restar}/>
-      <img src={imagenesCarousel[counter]?.cover_photo} alt="" className='sm:hidden md:w-[85%] md:h-[110vh] md:object-fit md:object-cover md:object-center  '/>
-      <img src={right} alt="" onClick={sumar}/>
+
+      <img src={imagenesCarousel[currentImage]?.cover_photo} alt="" className='sm:hidden md:w-[90%] md:h-[110vh] md:object-fit md:object-cover md:object-center  '/>
+
+    
     </div>
   )
 }
