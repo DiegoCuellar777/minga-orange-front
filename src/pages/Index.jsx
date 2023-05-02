a1152a15b6c6cbb059573f2e9f2b5dac42bd9d93
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-05-02 14:55:19 -0500

    M 04 01 (#14)
    * m-04-01 form chapter
    
    * m-04-01 New Chapter
    
    ---------
    
    Co-authored-by: Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Presentation from "../components/Presentation"
 import Carousel from '../components/Carousel'
 import Welcome from '../components/Welcome'
+import EditChapter from './EditChapter'
 
 export default function Index() {
     return (
         <main id="Home">
             {<Presentation />}
             <div className="min-h-[100vh] relative">
-                <Carousel/>
-                <Welcome/>
+                {/* <Carousel/>
+                <Welcome/> */}
+                <EditChapter/>
             </div>
         </main>
     )
