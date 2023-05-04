40033b2e8688570500c2de6114efe723626b19e8
DiegoCuellar777 <112589821+DiegoCuellar777@users.noreply.github.com>
2023-05-04 12:31:35 -0500

    Push final, todos los cambios y arreglados realizados para Sprint-2 (#25)

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import apiUrl from "../../api";
 import Swal from 'sweetalert2';
 
-export default function Register() {
+export default function LogIn(props) {
 
     const email = useRef()
     const password = useRef()
@@ -48,8 +48,8 @@ export default function Register() {
 
     return (
         <div className="flex">
-            <div className="bg-white w-[30%] flex flex-col justify-center items-center">
-                <img src="/public/images/Logo 2 1.png" className="h-[4rem] mb-[2rem]" alt="" />
+            <div className="bg-white w-[100%] sm:w-[30%] flex flex-col justify-center items-center">
+                <img src="/public/images/Logo 2 1.png" className="h-[4rem] my-[2rem]" alt="" />
                 <div className="flex flex-col items-center  mt-6">
                     <h2 className="font-bold text-center text-3xl/[39px]">Welcome!</h2>
                     <span className="text-center text-xs px-10 py-4">Discover manga, manhua and manhwa, track your progress, have fun, read manga.</span>
@@ -63,10 +63,17 @@ export default function Register() {
                     <button className="flex border w-[75%] py-3 rounded-lg border-[#1f1f1f78] justify-center ">
                         <img src="/public/images/Google.png" alt="" /><span>Sign in with Google</span>
                     </button>
-                    <span className="w-[75%] text-xs mt-4">you don't have an account yet? <Anchor to="/Register" className="text-red-600 font-bold">Sign up</Anchor></span>
+
+                    <div className="flex flex-col items-center">
+                        {props.setShow ? (
+                            <span className="w-[75%] text-xs mt-4">you don't have an account yet?  <span onClick={()=>props.setShow(false)} className="cursor-pointer text-red-600 font-bold">Log in</span></span>
+                        ) : (
+                            <Anchor to="/Register" className="w-[75%] text-xs mt-4">Already have an account? <span className="cursor-pointer text-red-600 font-bold">Sign in</span></Anchor>
+                        )}
+                    </div>
                 </div>
             </div>
-            <img src="/public/images/pexels-aleksandar-pasaric-2339009 2.png" className="w-[70%] h-screen object-cover" alt="" />
+            <img src="/public/images/pexels-aleksandar-pasaric-2339009 2.png" className="w-[70%] h-screen object-cover hidden sm:block" alt="" />
         </div>
     )
 }
