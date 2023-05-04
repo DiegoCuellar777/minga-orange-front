ebc6c379169ad073385fd6d7930c4a3e070e7c59
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-05-04 11:44:36 -0300

    edit ErroPage,router AuthorForm, page AuthorForm

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,10 +16,12 @@ export default function Nav() {
     const handleCloseOffcanvas = () => {
         setIsOffcanvasOpen(false);
     };
-    
+
     let token = localStorage.getItem("token")
-    let user = JSON.parse(localStorage.getItem("user"))
-    
+    let role = JSON.parse(localStorage.getItem("user"))?.role
+    let photo = JSON.parse(localStorage.getItem("user"))?.photo
+    let email = JSON.parse(localStorage.getItem("user"))?.email
+
     return (
         <nav className="h-[10vh] flex justify-between p-4 w-full fixed z-10 s border-gradient-to-r from-transparent to-transparent via-white">
             <button className="contents" onClick={handleMenuClick}>
@@ -43,12 +45,11 @@ export default function Nav() {
                 <div className="flex flex-col items-center w-[20rem] h-[100%] fixed bg-gradient-to-r from-neutral-700 to-black order-2 top-0 left-0 text-black z-50">
                     <div className="flex justify-between w-[100%] items-center">
                         {token && <div className="flex items-center p-2">
-                            <img className="bg-black w-[40px] h-[40px] rounded-[100%] mr-2" src={user?.photo} alt="" />
+                            <img className="bg-black w-[40px] h-[40px] rounded-[100%] mr-2" src={photo} alt="" />
                             <div className="ml-1 text-white">
-                                <h5>{user?.email}</h5>
+                                <h5>{email}</h5>
                             </div>
                         </div>}
-                        
                         <button className="text-white rounded p-2 w-[100%] flex justify-end " onClick={handleCloseOffcanvas}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -57,19 +58,21 @@ export default function Nav() {
                     </div>
                     <div className="w-[100%] h-[100%] text-white flex-col mt-3 cel:order-3 sm:order-2 flex items-center justify-between">
                         <ul className="w-[80%] flex flex-col items-center gap-2 text-center">
-                                                     
                             {!token && <Anchor to={'/LogIn'} className=" p-2 bg-white text-black rounded-md w-[70%]">Log In</Anchor>}
                             {!token && <Anchor to={'/register'} className=" p-2 bg-white text-black rounded-md w-[70%]">Register</Anchor>}
-                            {token &&<Anchor to={'/'} className=" p-2 bg-white text-black rounded-md w-[70%]">Home</Anchor>}
+                            {token && <Anchor to={'/'} className=" p-2 bg-white text-black rounded-md w-[70%]">Home</Anchor>}
                             {token && <Anchor to={'/manga-form'} className=" p-2 bg-white text-black rounded-md w-[70%]">Mangas</Anchor>}
                             {token && <Anchor className=" p-2 bg-white text-black rounded-md w-[70%]">My Mangas</Anchor>}
                             {token && <Anchor className=" p-2 bg-white text-black rounded-md w-[70%]">Favourites</Anchor>}
                             {token && <Anchor className=" p-2 bg-white text-black rounded-md w-[70%]">Logout</Anchor>}
-                            <Anchor to={'/chapter-form'} className=" p-2 bg-white text-black rounded-md w-[70%]">New Chapter</Anchor>
-
+                            {token && <Anchor to={'/chapter-form'} className=" p-2 bg-white text-black rounded-md w-[70%]">New Chapter</Anchor>}
                         </ul>
                         <ul className="w-[80%] flex justify-center items-center gap-2 text-center">
-                            {token &&<Link to="/author-form" className=" p-2 mb-4 bg-white text-black rounded-md w-[70%]">New Author</Link>}
+                            {token && role === 0 &&
+                                <Link to="/author-form" className="p-2 mb-4 bg-white text-black rounded-md w-[70%]">
+                                    New Author
+                                </Link>
+                            }
                         </ul>
                     </div>
                 </div>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,25 +1,34 @@
-import {createBrowserRouter} from "react-router-dom";
-import Main from "../layouts/Main.jsx"
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Main from "../layouts/Main.jsx";
 import Register from "../pages/Register.jsx";
-import LogIn from "../pages/LogIn.jsx"
-import Index from "../pages/Index";
+import LogIn from "../pages/LogIn.jsx";
+import Index from "../pages/Index.jsx";
 import AuthorForm from "../pages/AuthorForm.jsx";
 import MangaForm from "../pages/MangaForm.jsx";
 import EditChapter from "../pages/EditChapter.jsx";
-import MangaForm from '../pages/MangaForm.jsx'
 import ErrorPage from "../pages/ErrorPage.jsx";
 
-let token = localStorage.getItem("token")
+let token = localStorage.getItem("token");
+let role = JSON.parse(localStorage.getItem("user"))?.role;
 
 const routes = createBrowserRouter([
-    { path: "/", element:<Main/>, errorElement:<ErrorPage />, children:[
-        {path: "/", element: <Index/>},
-        {path: "/register", element: <Register/>},
-        {path: "/LogIn", element: token? <Index />: <LogIn />},
-        {path: "/author-form", element: <AuthorForm/>},
-        {path: "/manga-form", element: <MangaForm/>},
-        {path: "/chapter-form", element: <EditChapter/>},
-    ]}
-])
+    {
+        path: "/",
+        element: <Main />,
+        errorElement: <ErrorPage />,
+        children: [
+            { path: "/", element: <Index /> },
+            { path: "/register", element: <Register /> },
+            { path: "/LogIn", element: token ? <Index /> : <LogIn /> },
+            { path: "/author-form", element: role === 0 ? <AuthorForm /> : <Navigate to="/" /> },
+            { path: "/manga-form", element: <MangaForm /> },
+            { path: "/chapter-form", element: <EditChapter /> },
+        ],
+    },
+    {
+        path: "*", // cualquier ruta desconocida
+        element: <Navigate to="/" />, // redirigir a la página de inicio
+    },
+]);
 
-export default routes
+export default routes;
