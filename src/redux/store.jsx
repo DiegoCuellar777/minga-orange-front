0a0a6ed24249c54f7d96b73ecf0dd3d310e0cf0b
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2023-05-11 11:01:02 -0300

    mangaRe mangaAction botones next y prev falta redux, click manga (#34)

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mangas from './reducers/mangas'
+import mangaCards from './reducers/mangaCards'
 import save_author from "../redux/reducers/save_author"
 
 const store = configureStore({
     reducer: {
         save_author: save_author,
-        inputManga: mangas
+        inputManga: mangas,
+        pageMangas: mangaCards
 
     }
 })
