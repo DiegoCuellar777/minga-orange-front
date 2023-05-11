262743a4bcc3a53696035c3167424db20ba838d5
DiegoCuellar777 <diegocuellar7@gmail.com>
2023-05-11 10:11:32 -0500

    Final M-09

--- src/redux/actions/chapterData.js
+++ src/redux/actions/chapterData.js
@@ -1,15 +1,28 @@
 import { createAction } from "@reduxjs/toolkit";
 
+// la accion es un intermediario entre la vista y las operaciones de reduccion
+// es la que dispara /ejecuta la modificacion, reduccion de los estados globales
 const chapterData = createAction(
-    "chapterData",
-    (object) => {
-
+    "chapterData",      // nombre de la accion
+    (object) => {       // funcion que va a enviar datos al reductor
+                        // el objeto debe tener todas las propiedades a guardarse
+                        // en el estado global
         return {
             payload:{
                 title: object.title,
-                page: object.page,
-                _id: object._id
+                pageRef: object.pageRef,
+                _id: object._id,
+                manga_id: object.manga_id
             }
         }
     }
 )
+// Unico objetivo: enviar informacion al reductor
+// En el reductor se realiza toda la logica necesaria para modificar/reducir los estados globales
+
+const actions = { chapterData }
+// construye un objeto con la accion (mas adelante se irán agregando más acciones)
+export default actions
+
+// Se exporta para poder utilizarlo en los componentes que van a despachar los datos 
+// y para configurar la logica del reductor
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mangas from './reducers/mangas'
 import save_author from "../redux/reducers/save_author"
+import chapterData from "./reducers/chapterData"
 
 const store = configureStore({
     reducer: {
         save_author: save_author,
-        inputManga: mangas
-
+        inputManga: mangas,
+        dataChapter: chapterData
     }
 })
 
