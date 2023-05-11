c6568a63368c5de564c054ec48574bb96745689b
Carlos Arturo Torres <carlosat95@gmail.com>
2023-05-11 10:03:27 -0500

    Redux y chapters, links

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,15 +6,17 @@ import { useSelector } from "react-redux";
 import save_author from "../redux/actions/save_author";
 const { saveAuthorData } = save_author
 const { saveMangas } = save_author
+import MangaDetails from './MangaDetails';
 
 export default function Index() {
-    const store = useSelector(store => console.log(store.save_author))
+    //const store = useSelector(store => console.log(store.save_author))
     return (
         <main id="Home">
             {<Presentation />}
             <div className="min-h-[100vh] relative md:w-full md:flex md:items-center md:justify-around">
-                <Carousel/>
-                <Welcome/>
+                {/* <Carousel/>
+                <Welcome/> */}
+                <MangaDetails/>
             </div>
         </main>
     )
