2c615d382ec09f269a294e7a14e48f697b3de91d
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-05-11 10:05:04 -0500

    Redux y chapters, links (#35)

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -2,51 +2,69 @@ import React from 'react'
 import pru from '../assets/images/image4.png'
 import axios from 'axios'
 import { useEffect, useState, useRef } from 'react'
+import { Link as Anchor, useNavigate } from "react-router-dom"
 import apiUrl from '../../api.js'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Chapters() {
-  const {cover_photo,title,manga_id} = useSelector(store => store.inputManga)
+  const { cover_photo, title, manga_id } = useSelector(store => store.inputManga)
   console.log(cover_photo)
-  //let manga_id = '6453ed47cf3118bcb0e205a8'
-  //let [img, setImg] = useState()
-  //let [title, setTitle] = useState('')
-  let [chapter,setChapters] = useState([])
-  //let [details, setDetails] = useState('')
+  let [chapter, setChapters] = useState([])
+  let [page, setPage] = useState(1)
   useEffect(
     () => {
-      axios(apiUrl+`chapters/get?manga_id=${manga_id}&page=${1}&limit=${4}`)
-      .then(res => {
-        setChapters(res.data.response)
-        console.log('Este es axios ',res.data.response.cover_photo)
-        //setImg(res.data.response.cover_photo)
-        //setTitle(res.data.response.title)
-      })
-      .catch(err => console.log(err))
+      axios(apiUrl + `chapters/get?manga_id=${manga_id}&page=${page}&limit=${4}`)
+        .then(res => {
+          setChapters(res.data.response)
+          console.log('Este es axios ', res.data.response.cover_photo)
+
+        })
+        .catch(err => console.log(err))
     },
-    []
-  ) 
-  console.log('Este es chapter ',chapter)
-  chapter.map(each => console.log(typeof each?.cover_photo))
+    [page]
+  )
+
+  let next = () => {
+    setPage(page + 1)
+    console.log(page)
+  }
+
+  let prev = () => {
+    setPage(page - 1)
+    console.log(page)
+  }
+
+  chapter.map(each => console.log(each?._id))
+
 
   return (
     <>
       <div className='flex flex-col items-center justify-around h-screen'>
         <div className='container w-[90rem] h-[30rem] bg-cover bg-center' style={{ backgroundImage: `url(${cover_photo})` }}>
-          <div className='flex flex-col justify-center items-center h-full'>
-            <p className='text-white text-xl'>Chapter of</p>
-            <h3 className='text-white text-5xl font-bold mb-10'>Titulo manga</h3>
-            <button className='bg-[#ffffff2a] rounded-[5px] p-2 flex items-center border-[1px] border-[#ffffff1e] w-[30rem] lg:bg-[#ffffff1e]'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-7 h-7 ml-1">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
-              <p className='text-white text-lg font-thin ml-2'>Search</p>
-            </button>
+          <div className=' bg-[#0000007a] h-[100%]'>
+            <div className='flex flex-col justify-center items-center h-full'>
+              <p className='text-white text-xl'>Chapter of</p>
+              <h3 className='text-white text-5xl font-bold mb-10'>{title}</h3>
+              <button className='bg-[#ffffff2a] rounded-[5px] p-2 flex items-center border-[1px] border-[#ffffff1e] w-[30rem] lg:bg-[#ffffff1e]'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-7 h-7 ml-1">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+                <p className='text-white text-lg font-thin ml-2'>Search</p>
+              </button>
+            </div>
           </div>
         </div>
 
-        <div className='flex justify-around'>
-          {chapter?.map(each => <img key={each._id} className='p-3 m-2 w-52' src={each.cover_photo} alt="" /> )}
+        <div className='flex justify-around items-center'>
+
+          {page > 1 ? <svg onClick={prev} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+          </svg> : <div></div>}
+          {chapter?.map(each => <Anchor key={each._id}  to={`/chapters/${each._id}/${0}`} > <img className='p-3 m-2 w-52' src={each.cover_photo} alt="" /> </Anchor>)}
+
+          {chapter.length >= 4 ? <svg onClick={next} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg> : <div></div>}
         </div>
       </div>
     </>
--- src/pages/MangaDetails.jsx
+++ src/pages/MangaDetails.jsx
@@ -1,18 +1,26 @@
 import axios from 'axios'
-import { Link as Anchor, useNavigate} from "react-router-dom"
+import { Link as Anchor, useNavigate } from "react-router-dom"
 import { useEffect, useState, useRef } from 'react'
 import apiUrl from '../../api.js'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import mangasActions from '../redux/actions/mangas'
 
 export default function MangaDetails() {
-  const {readOneManga} = mangasActions
-  const store = useSelector(store => console.log('STORE ',store))
+  const { readOneManga } = mangasActions
+  const store = useSelector(store => console.log('STORE ', store))
+  let token = localStorage.getItem('token')
+  let headers = {headers:{'Authorization':`Bearer ${token}`}}
   const dispatch = useDispatch()
-  let manga_id = '6453ed48cf3118bcb0e205bf'
+  let manga_id = '6453ed47cf3118bcb0e205a8'
   let [img, setImg] = useState()
   let [title, setTitle] = useState('')
   let [details, setDetails] = useState('')
+  let [authorIp, setAuthorIp] = useState()
+  let [categoryId, setCategoryId] = useState()
+  let [nameAuthor, setnameAthor] = useState('')
+  let [lastAuthor, setlastAthor] = useState('')
+  let [category, setCategory] = useState('')
+  let [photo, setPhoto] = useState('')
   useEffect(
     () => {
       axios(apiUrl + `mangas/${manga_id}`)
@@ -20,6 +28,8 @@ export default function MangaDetails() {
           setImg(res.data.response.cover_photo)
           setTitle(res.data.response.title)
           setDetails(res.data.response.description)
+          setAuthorIp(res.data.response.author_id)
+          setCategoryId(res.data.response.category_id)
           dispatch(readOneManga({
             cover_photo: res.data.response.cover_photo,
             title: res.data.response.title,
@@ -29,6 +39,37 @@ export default function MangaDetails() {
         .catch(err => console.log(err))
     },
     []
+  )
+  useEffect(
+    () => {
+      axios(apiUrl + `api/authors/${authorIp}`,headers)
+        .then(res => {
+          setnameAthor(res.data.response?.name)
+          setlastAthor(res.data.response?.last_name)
+          setPhoto(res.data.response.photo)
+          console.log(authorIp)
+        })
+        .catch(err => console.log(err))
+    },
+    [authorIp]
+  )
+
+  useEffect(
+    () => {
+      axios(apiUrl + `categories`)
+        .then(res => {
+          let ca = res.data.categories
+          let category = ca.find(category => {
+            if (category._id == categoryId) {
+              return category.name
+            }
+          })
+          console.log('Esta Wea ', category)
+          setCategory(category.name)
+        })
+        .catch(err => console.log(err))
+    },
+    [categoryId]
   )
 
   return (
@@ -41,8 +82,8 @@ export default function MangaDetails() {
         <div className='absolute left-0 top-0 xl:w-[472px] lg:w-[350px] h-[335px] bg-gradient-to-r from-[#4338CA] to-[#5E52F3] blur-[304px] rounded-full'></div>
         <div className='flex-none flex-col items-center break-words'>
           <div className='flex justify-between text-white'>
-            <p>shonen</p>
-            <p><img src="" alt="" /> Nombre autor</p>
+            <p>{category}</p>
+            <Anchor to={`/authors/${authorIp}`} className='flex'><img className='w-[30px] p-1' src={photo} alt="" /> {nameAuthor} {lastAuthor}</Anchor>
           </div>
           <h3 className='flex items-center tracking-wider leading-[140%] font-semibold w-auto text-white text-left xl:text-[48px] lg:text-[35px]'>
             {title}
@@ -50,11 +91,9 @@ export default function MangaDetails() {
           <p className='mt-5 flex items-center tracking-wider leading-[120%] font-normal not-italic w-auto text-white text-left xl:text-[16px] lg:text-[15px]'>
             {details}
           </p>
-          <Anchor img={img} to={'/chapters/get'} className="flex justify-center mt-5  rounded-[5px] text-gray-500 bg-gradient-to-r from-[#4338CA] to-[#5E52F3]  xl:text-[16px] lg:text-[15px]">Read Now</Anchor>
         </div>
+        <Anchor img={img} to={'/chapters/get'} className="flex justify-center mt-5  rounded-[5px] text-gray-500 bg-gradient-to-r from-[#4338CA] to-[#5E52F3]  xl:text-[16px] lg:text-[15px]">Read Now</Anchor>
       </div>
-
-
     </>
   )
 }
