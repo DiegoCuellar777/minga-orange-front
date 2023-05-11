0800129f2a1fc80eb669712a0c8df25aed6fb306
Nicolas <nilonb7@gmail.com>
2023-05-11 17:11:20 -0300

    m-07-v2

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,13 +3,15 @@ import mangas from './reducers/mangas'
 import mangaCards from './reducers/mangaCards'
 import save_author from "../redux/reducers/save_author"
 import chapterData from "./reducers/chapterData"
+import inputs_mangaForm from './reducers/inputs_mangaForm'
 
 const store = configureStore({
     reducer: {
         save_author: save_author,
         inputManga: mangas,
         dataChapter: chapterData,
-        pageMangas: mangaCards
+        pageMangas: mangaCards,
+        mangaForn: inputs_mangaForm,
     }
 })
 
